d1ae01e83c7d91699396de4092f6dc582be94765
aurimasko <aurimasko@gmail.com>
2021-09-03 22:18:33 +0300

    database added

--- EmployeesAPI/Startup.cs
+++ EmployeesAPI/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeesAPI.Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,16 @@ namespace EmployeesAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<EmployeesDbContext>(options =>
+            {
+                string connectionString = Configuration.GetConnectionString("EmployeesDatabase");
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString),
+                    (settings) =>
+                    {
+                        settings.EnableRetryOnFailure();
+                    }).UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
