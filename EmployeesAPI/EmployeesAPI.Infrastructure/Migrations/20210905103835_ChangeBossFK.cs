883be8b1e558500b320261ddbe153a20dd1a5890
aurimasko <aurimasko@gmail.com>
2021-09-05 13:48:41 +0300

    * EmployeesController.cs: added add employee endpoint.

---
+++ EmployeesAPI/EmployeesAPI.Infrastructure/Migrations/20210905103835_ChangeBossFK.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeesAPI.Infrastructure.Migrations
+{
+    public partial class ChangeBossFK : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Employees_BossId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "BossId",
+                table: "Employees",
+                type: "char(36)",
+                nullable: true,
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "char(36)")
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Employees_BossId",
+                table: "Employees",
+                column: "BossId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Employees_BossId",
+                table: "Employees");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "BossId",
+                table: "Employees",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci",
+                oldClrType: typeof(Guid),
+                oldType: "char(36)",
+                oldNullable: true)
+                .OldAnnotation("Relational:Collation", "ascii_general_ci");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Employees_BossId",
+                table: "Employees",
+                column: "BossId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
